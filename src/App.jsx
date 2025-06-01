726b892febfade0a35a58cd8f7cfbc2b533f3ab1
KashyapPLN <kashyap.pln@gmail.com>
2025-06-01 14:10:14 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Landing from './Components/Landing/Landing'
+import { Route, Routes } from 'react-router-dom'
+import MenuPage from './Components/MenuPage/MenuPage'
 
 function App() {
 
 
   return (
     <>
-      <Navbar/>
-      <Landing/>
+      <Navbar/>      
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+       <Route path='/menu' element={<MenuPage/>}/>
+      </Routes>
     </>
   )
 }
